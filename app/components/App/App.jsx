79cd85bb89aeb25e903cd5135e9c6de02fd25702
piousbox <piousbox@gmail.com>
2017-03-07 17:22:57 -0800

    lets go react bootstrap of sorts

---
+++ app/components/App/App.jsx
@@ -0,0 +1,42 @@
+import styles from './_App.scss';
+
+import React from 'react';
+import AppActions from '../../actions/AppActions';
+import ItemsStore from '../../stores/ItemsStore';
+import Body from '../Body/Body';
+import Footer from '../Footer/Footer';
+import Nav from '../Nav/Nav';
+
+function getAppState() {
+  return {
+    items: ItemsStore.getAll()
+  };
+}
+
+export default class App extends React.Component {
+
+  state = getAppState()
+
+  componentDidMount() {
+    ItemsStore.addChangeListener(this.onChange);
+    AppActions.getItems();
+  }
+
+  componentWillUnmount() {
+    ItemsStore.removeChangeListener(this.onChange);
+  }
+
+  onChange = () => {
+    this.setState(getAppState());
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <Nav />
+        <Body items={this.state.items} />
+        <Footer />
+      </div>
+    );
+  }
+}
---
+++ app/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+// import styles from './_Footer.scss';
+import React from 'react';
+
+export default class Nav extends React.Component {
+  render() {
+    return (
+        <div>
+          HERE BE HEADER
+        </div>
+    );
+  }
+}
