1e61fb6d921cedf4529fe8fb0a677ca2d14cdd64
piousbox <piousbox@gmail.com>
2017-03-08 06:01:09 -0800

    step by step, muiTheme is in.

--- app/components/MainNavigation/MainNavigation.jsx
+++ app/components/MainNavigation/MainNavigation.jsx
@@ -2,12 +2,27 @@
 // import styles from './_Footer.scss';
 import React from 'react';
 
+import { AppBar, Tabs, Tab } from 'material-ui';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 export default class MainNavigation extends React.Component {
-  render() {
-    return (
-      <div>
-        Wasya co [About] [Knowledge Base] [Services] [Our Process] [Our Technology] [Clients] [Contact]
-      </div>
+    getChildContext() {
+        return { muiTheme: getMuiTheme(baseTheme) };
+    }
+
+    render() {
+        return (
+            <AppBar title="My App" >
+              <div>
+                Wasya co [About] [Knowledge Base] [Services] [Our Process] [Our Technology] [Clients] [Contact]
+        </div>
+        
+      </AppBar>
     );
   }
 }
+
+MainNavigation.childContextTypes = {
+    muiTheme: React.PropTypes.object.isRequired,
+};
