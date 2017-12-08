5ed09257f40a59b822a49a88f4c10934302f14a6
piousbox <piousbox@gmail.com>
2017-12-08 14:35:11 -0800

    team looks better

--- app/components/App/Team.jsx
+++ app/components/App/Team.jsx
@@ -11,76 +11,100 @@ import SectionHeader from './SectionHeader'
 import victor  from './images/team/victor_500x600.jpg'
 import oleg    from './images/team/oleg_500x600.jpg'
 import kim     from './images/team/kim_500x600.jpg'
+import brad    from './images/team/brad_500x600.jpg'
+import steve   from './images/team/steve_500x600.jpg'
+import bondar  from './images/team/bondar_500x600.jpg'
+
+class TeamMember extends React.Component {
+  render () {
+    let description = []
+    this.props.member.description.map((p,idx) => {
+      description.push(<p key={idx} >{p}</p>)
+    })
+    return (
+      <div className="dez-box">
+        <div className="dez-media"> <a href="#"><img alt="" src={this.props.member.image} /></a> </div>
+        <div className="dez-info p-a20 p-t40 border-1">
+          <h4 className="dez-title m-tb0"><a href="#">{this.props.member.name}</a></h4>
+          <div className="bg-primary skew-content-box">
+            <ul className="dez-social-icon">
+              <li><a href="javascript:void(0);" className="fa fa-github"></a></li>
+              <li><a href="javascript:void(0);" className="fa fa-linkedin"></a></li>
+            </ul>
+          </div>
+          <p><em>{this.props.member.title}</em></p>
+          { description }
+        </div>
+        <br /><br />
+      </div>
+    )
+  }
+}
 
 class Team extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { team: [
-      { image: victor,
-        name: 'Victor Pudeyev',
-        title: 'Engineer',
-        description: [
-          "Victor Piousbox, the great russian bear rider, owner of wooden rubles, conqueror of rivers and dataflows, fearless thinker and tinker, has been with Operaevent since the beginning. He has been honestly producing the technical foundation on which the platform relies for a while now. Before then, he has rotated many software engineering roles in a variety of companies in the Bay Area and elsewhere. In his spare time, he likes to run around in the forest shooting guns in the manner of a sport exercise, ride his motorcycle into the sunset, and read technical documentation to better his skill in technology bending.",
-          "A full-stack engineer focusing on Ruby on Rails, Javascript, devops, and web application development for startups and agile teams."
-        ],
+    this.state = { team: {
+      victor: { image: victor,
+                name: 'Victor Pudeyev',
+                title: 'Principal Software Developer',
+                description: [
+                  "Victor Piousbox, the great russian bear rider, owner of wooden rubles, conqueror of rivers and dataflows, fearless thinker and tinker, has been with Operaevent since the beginning. He has been honestly producing the technical foundation on which the platform relies for a while now. Before then, he has rotated many software engineering roles in a variety of companies in the Bay Area and elsewhere. In his spare time, he likes to run around in the forest shooting guns in the manner of a sport exercise, ride his motorcycle into the sunset, and read technical documentation to better his skill in technology bending.",
+                  "A full-stack engineer focusing on Ruby on Rails, Javascript, devops, and web application development for startups and agile teams."
+                ],
       },
-      { image: kim,
-        name: 'Kim Sinton',
-        title: 'Advisor',
-        description: [
-          "Kim is a front-end engineer with a vast professional experience. He finds innovative strategic solutions to the growing pains of the team and contributes invaluably to our overall vision."
-        ],
+      kim: { image: kim,
+             name: 'Kim Sinton',
+             title: 'Principal Software Developer',
+             description: [
+               "Kim is a front-end engineer with a vast professional experience. He finds innovative strategic solutions to the growing pains of the team and contributes invaluably to our overall vision."
+             ],
       },
-      { image: oleg,
-        name: 'Oleg Pudeyev',
-        title: 'Engineer',
-        description: [
-          "Full stack engineer and tech lead with 12 years of Web development experience with a focus on Ruby on Rails applications. Experienced in hands-on development, design and architecture, technical leadership, mentoring, project management and planning.",
-          " My objective is to write code that is beautiful, maintainable and delights customers when it is deployed."
-        ],
+      oleg: { image: oleg,
+              name: 'Oleg Pudeyev',
+              title: 'Principal Software Developer',
+              description: [
+                "Full stack engineer and tech lead with 12 years of Web development experience with a focus on Ruby on Rails applications. Experienced in hands-on development, design and architecture, technical leadership, mentoring, project management and planning.",
+                "His objective is to write code that is beautiful, maintainable and delights customers when it is deployed."
+              ],
       },
-
-    ] }
+      brad: { image: brad,
+              name: 'Bradford Prothman-Roberts',
+              title: 'Economics Advisor',
+              description: [ 'Brad is a University of South Carolina graduate. He participates in studies of product feasibility, as well as import/export to Latin America.' ],
+      },
+      steve: {
+        image: steve,
+        name: 'Stephen Kim',
+        title: 'Legal Advisor',
+        description: [ 'Stephen is the legal advisor to the team.' ],
+      },
+      bondar: {
+        image: bondar,
+        name: 'Julia Bondar',
+        title: 'Cultural Advisor',
+        description: [ 'Julia is a cultural advisor to the team. Occasionally, she acts in the role of HR.' ],
+      },
+    } }
   }
 
-  render () {
-    let cols = []
-    this.state.team.map((i, p) => {
-      let descr = []
-      i.description.map((j, k) => {
-        descr.push(<p key={k} className="m-t10 m-b0 text-justify">{j}</p>)
-      })
-      cols.push(          
-        <Col key={p} 
-             xs={8} xsOffset={2} 
-             sm={6} smOffset={0} 
-             md={4} >
-          <div className="dez-box">
-            <div className="dez-media"> <a href="#"><img alt="" src={i.image} /></a> </div>
-            <div className="dez-info p-a20 p-t40 border-1">
-              <h4 className="dez-title m-tb0"><a href="#">{i.name}</a></h4>
-              <div className="bg-primary skew-content-box">
-                <ul className="dez-social-icon">
-                  <li><a href="javascript:void(0);" className="fa fa-github"></a></li>
-                  <li><a href="javascript:void(0);" className="fa fa-linkedin"></a></li>
-                </ul>
-              </div>
-              <span>{i.title}</span>
-              { descr }
-            </div>
-          </div>
-          <br /><br />
-        </Col>
-      )
-    })
-    
+  render () {    
     return (
       <Grid id="teamContent" >
         <Row>
           <Col xs={12}>
             <SectionHeader>Our Team</SectionHeader>
           </Col>
-          { cols }
+        </Row>
+        <Row>
+          <Col xs={8} xsOffset={2} sm={6} smOffset={3} md={4} mdOffset={0} ><TeamMember member={this.state.team.victor} /></Col>
+          <Col xs={8} xsOffset={2} sm={6} smOffset={3} md={4} mdOffset={0} ><TeamMember member={this.state.team.kim} /></Col>
+          <Col xs={8} xsOffset={2} sm={6} smOffset={3} md={4} mdOffset={0} ><TeamMember member={this.state.team.oleg} /></Col>
+        </Row>
+        <Row>
+          <Col xs={8} xsOffset={2} sm={6} smOffset={3} md={4} mdOffset={0} ><TeamMember member={this.state.team.brad} /></Col>
+          <Col xs={8} xsOffset={2} sm={6} smOffset={3} md={4} mdOffset={0} ><TeamMember member={this.state.team.steve} /></Col>
+          <Col xs={8} xsOffset={2} sm={6} smOffset={3} md={4} mdOffset={0} ><TeamMember member={this.state.team.bondar} /></Col>
         </Row>
       </Grid>
     )
