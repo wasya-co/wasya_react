715ccad7bf0a4a165a3ddb8e88c75d2cc5e21afd
piousbox <piousbox@gmail.com>
2017-12-12 13:17:29 -0800

    links in team

--- app/components/App/Team.jsx
+++ app/components/App/Team.jsx
@@ -21,17 +21,20 @@ class TeamMember extends React.Component {
     this.props.member.description.map((p, idx) => {
       description.push(<p key={idx} >{p}</p>)
     })
+
+    let icons = []
+    if (this.props.member.links) {
+      this.props.member.links.map((icon, idx) => {
+        icons.push(<li><a href={icon.link} className={`fa ${icon.icon}`}></a></li>)
+      })
+    }
+
     return (
-      <div className="dez-box">
+      <div className="dez-box" >
         <div className="dez-media"> <a href="#"><img alt="" src={this.props.member.image} /></a> </div>
         <div className="dez-info p-a20 p-t40 border-1">
           <h4 className="dez-title m-tb0"><a href="#">{this.props.member.name}</a></h4>
-          <div className="bg-primary skew-content-box">
-            <ul className="dez-social-icon">
-              <li><a href="javascript:void(0);" className="fa fa-github"></a></li>
-              <li><a href="javascript:void(0);" className="fa fa-linkedin"></a></li>
-            </ul>
-          </div>
+          { icons.length > 0 ? <div className="bg-primary skew-content-box"><ul className="dez-social-icon">{ icons }</ul></div> : null }
           <p><em>{this.props.member.title}</em></p>
           { description }
         </div>
@@ -52,12 +55,20 @@ class Team extends React.Component {
                   "Victor Piousbox, the great russian bear rider, owner of wooden rubles, conqueror of rivers and dataflows, fearless thinker and tinker, has been with Operaevent since the beginning. He has been honestly producing the technical foundation on which the platform relies for a while now. Before then, he has rotated many software engineering roles in a variety of companies in the Bay Area and elsewhere. In his spare time, he likes to run around in the forest shooting guns in the manner of a sport exercise, ride his motorcycle into the sunset, and read technical documentation to better his skill in technology bending.",
                   "A full-stack engineer focusing on Ruby on Rails, Javascript, devops, and web application development for startups and agile teams."
                 ],
+                links: [
+                  { icon: 'fa-file-pdf-o', title: 'Resume',   link: 'https://s3.amazonaws.com/ish-archive/2017/hunter/201712-VictorPudeyev-Resume.pdf' },
+                  { icon: 'fa-github',     title: 'Github',   link: 'https://github.com/piousbox' },
+                  { icon: 'fa-linkedin',   title: 'LinkedIn', link: 'https://www.linkedin.com/in/victor-pudeyev-ba9008107/' },
+                ],
       },
       kim: { image: kim,
              name: 'Kim Sinton',
-             title: 'Principal Software Developer',
+             title: 'Software Developer',
              description: [
                "Kim is a front-end engineer with a vast professional experience. He finds innovative strategic solutions to the growing pains of the team and contributes invaluably to our overall vision."
+             ],
+             links: [
+               { icon: 'fa-linkedin', title: 'LinkedIn', link: 'https://www.linkedin.com/in/kim-sinton-3a8848/', },
              ],
       },
       oleg: { image: oleg,
@@ -67,11 +78,19 @@ class Team extends React.Component {
                 "Full stack engineer and tech lead with 12 years of Web development experience with a focus on Ruby on Rails applications. Experienced in hands-on development, design and architecture, technical leadership, mentoring, project management and planning.",
                 "His objective is to write code that is beautiful, maintainable and delights customers when it is deployed."
               ],
+              links: [
+                { icon: 'fa-file-pdf-o',  title: 'Resume',   link: 'http://olegp.com/resume/oleg-pudeyev-resume.pdf' },
+                { icon: 'fa-github',   title: 'Github',   link: 'https://github.com/p' },
+                { icon: 'fa-linkedin', title: 'LinkedIn', link: 'https://www.linkedin.com/in/olegpudeyev/' },
+              ],
       },
       brad: { image: brad,
               name: 'Bradford Prothman-Roberts',
-              title: 'Economics Advisor',
+              title: 'Economic Advisor',
               description: [ 'Brad is a University of South Carolina graduate. He participates in studies of product feasibility, as well as import/export to Latin America.' ],
+              links: [
+                { icon: 'fa-linkedin', title: 'LinkedIn', link: 'https://www.linkedin.com/in/broadfjord/' },
+              ],
       },
       steve: {
         image: steve,
@@ -98,8 +117,8 @@ class Team extends React.Component {
         </Row>
         <Row>
           <Col xs={8} xsOffset={2} sm={6} smOffset={3} md={4} mdOffset={0} ><TeamMember member={this.state.team.victor} /></Col>
-          <Col xs={8} xsOffset={2} sm={6} smOffset={3} md={4} mdOffset={0} ><TeamMember member={this.state.team.kim} /></Col>
           <Col xs={8} xsOffset={2} sm={6} smOffset={3} md={4} mdOffset={0} ><TeamMember member={this.state.team.oleg} /></Col>
+          <Col xs={8} xsOffset={2} sm={6} smOffset={3} md={4} mdOffset={0} ><TeamMember member={this.state.team.kim} /></Col>
         </Row>
         <Row>
           <Col xs={8} xsOffset={2} sm={6} smOffset={3} md={4} mdOffset={0} ><TeamMember member={this.state.team.brad} /></Col>
