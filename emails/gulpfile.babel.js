25b7db063f4c5ae240b028859517bd6e48caa96d
piousbox <piousbox@gmail.com>
2018-01-11 17:40:17 -0800

    lets work on marketing-react.html

--- emails/gulpfile.babel.js
+++ emails/gulpfile.babel.js
@@ -56,7 +56,8 @@ function pages() {
       root: 'src/pages',
       layouts: 'src/layouts',
       partials: 'src/partials',
-      helpers: 'src/helpers'
+      helpers: 'src/helpers',
+      data: 'src/data/'
     }))
     .pipe(inky())
     .pipe(gulp.dest('dist'));
@@ -108,7 +109,7 @@ function server(done) {
 // Watch for file changes
 function watch() {
   gulp.watch('src/pages/**/*.html').on('all', gulp.series(pages, inline, browser.reload));
-  gulp.watch(['src/layouts/**/*', 'src/partials/**/*']).on('all', gulp.series(resetPages, pages, inline, browser.reload));
+  gulp.watch(['src/layouts/**/*', 'src/partials/**/*', 'src/data/**/*']).on('all', gulp.series(resetPages, pages, inline, browser.reload));
   gulp.watch(['../scss/**/*.scss', 'src/assets/scss/**/*.scss']).on('all', gulp.series(resetPages, sass, pages, inline, browser.reload));
   gulp.watch('src/assets/img/**/*').on('all', gulp.series(images, browser.reload));
 }
