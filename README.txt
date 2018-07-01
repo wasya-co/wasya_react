40fe2b9c48a4538f4dc973e1a4c1594f12d86e07
v p <piousbox@vs-MacBook-Pro.local>
2018-07-01 12:03:31 -0700

    sturfee case study

--- README.txt
+++ README.txt
@@ -16,4 +16,4 @@ don't forget that the index.html is in build/ dir!
  sudo apt-get install php-curl php7.0-curl -y
  sudo apt-get install php5.6-curl -y
 
-port 8000
+10.0.0.47:8000/
--- app/components/Blog/index.jsx
+++ app/components/Blog/index.jsx
@@ -29,7 +29,47 @@ let Items = {
         <p>My team was approached to do the redesign and rewrite of the existing tool. We chose a different stack - went from Python and SQL to Ruby and MongoDB. We chose to implement the frontend in jQuery, rather than React for this client. We deployed on CenturyLink cloud using lean custom deploy scripts. AWS S3 was used in this stack for object storage.</p>
         <p>The project took several months and was completed successfully. It's in maintenance mode, and my team does rolling feature augmentation and bug fixes.</p>
         <p>Since the SMT is performing well in the company, there are plans to license it out to Creek's peers and companies that need a similar-functioning tool. The architecture of the tool is modular enough that it will make licensing, and even on-premise installation (for audit and security reasons) a breeze.</p>
-    </div>) 
+    </div>),
+  },
+  'sturfee-build-pipeline': {
+    name: 'Sturfee Build Pipeline',
+    slug: 'sturfee-build-pipeline',
+    created_at: '2018-07-01',
+    subhead: '',
+    photo: w,
+    description: (
+      <div>
+        <p>Overview</p>
+        <p>I consulted for Sturfee in 2016.</p>
+        <p>Sturfee is an engineering-heavy small startup team developing products in AI space. As data scientists and scientists, the team was is knowledgable about AI and ML, however they lack experience in software development, and knowledge of best practices. Sturfee reached out to me to setup process for building their software, managing environments, codebases and versions, and implement an agile development cycle.</p>
+        <p>The Problem</p>
+        <ul>
+          <li>Multiple environments, multiple versions, multiple branches of multiple codebases</li>
+          <li>Microservice architecture necessitates a higher degree of infrastructure management than monolithic applications</li>
+          <li>Junior team members not following naming conventions</li>
+          <li>Hard-coded environmental variable in the codebase</li>
+        </ul>
+        <p>The Stack</p>
+        <ul>
+          <li>Angular Frontend</li>
+          <li>Go microservices</li>
+          <li>Node microservices</li>
+          <li>Python microservices</li>
+          <li>All hosted on AWS</li>
+        </ul>
+        <p>The Solution</p>
+        <p>It was critical to separate configuration from the codebase. I saw that some variables, such as database names and port numbers, were actually hard-coded into production code, sometimes with if-then statements that got clumsy and unmaintainable extremely rapidly. I modularized the configuration, moved it to separate data containers (json files files),&nbsp; removed these files from code repositories, and started managing these config files with infra orchestration tool (chef).</p>
+        <p>I introduced naming conventions and semantic versioning. Version number of every codebase became semantic and readable: just by looking at the branch name (correlated to the version), one is able to see what branch is the latest, and belongs to which proposed release. My methodology is losely based on git flow and semver, however see &lt;this-article&gt; for the complete description of git versioning that I recommend.</p>
+        <ul>
+          <li>Opscode Chef</li>
+          <li>Jenkins</li>
+          <li>Semantic Versioning</li>
+          <li>Custom build scripts that allow push-button, repeatable &amp; accountable deploy process</li>
+          <li>Branching strategy that involved a stable master branch and rolling release branches, plus feature braches that get merged into release branches (that are tracked by the master branch, which is presumably the latest stable).</li>
+        </ul>
+        <p>See the <a href="https://s3.amazonaws.com/ish-archive/2016/201603Q1_sturfee_build_pipeline.pdf">Sturfee Build Pipeline whitepaper</a> for additional information on this project.</p>
+    </div>
+    ),
   },
 }
 
@@ -195,7 +235,7 @@ class BlogItem extends React.Component {
           <Row>
             <Col md={9} sm={12} className="m-b30">
               <br />
-              { /* <h1 className="m-b20 m-t0">{ item.name }</h1> */ }
+              <h1 className="m-b20 m-t0">{ item.name }</h1>
               <div className="dez-separator bg-primary"></div>
               <div><em>{item.created_at ? `On ${item.created_at}` : null}</em></div>
               <img style={{ padding: '1rem' }} src={item.photo && item.photo.large_url} alt='' />
